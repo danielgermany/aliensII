9057f9a2f4a66cf5cd28e7f3eea849b150921ebf
csPython <cspython@AD25TF04NGF1J-89.local>
2018-12-03 10:36:52 -0500

    addomg version 4 of code

---
+++ alienDG04.py
@@ -0,0 +1,35 @@
+#Create variables
+pop = int
+time = int
+
+#Introduce user to program
+print("Aliens invaded Earth!")
+print("In this simulation we can assume that aliens give birth to 1 new alien baby every week")
+print("We can also assume that a baby alien grows into adulthood in a week")
+print("These aliens are also immortal")
+while True:
+    #Ask for user input
+    print("How many aliens landed on Earth?")
+    pop = input()
+    try:
+        pop = int(pop)
+        break
+    except ValueError:
+        print("The answer given is not a valid number")
+while True:
+    print("How long are they invading for? (In weeks)")
+    time = input()
+    try:
+        time = int(time)
+        break
+    except ValueError:
+        print("The answer given is not a valid number")
+        
+if pop == 1:
+    print("There is only 1 alien in week 0")
+else:
+    print("In week 0 there are " + str(pop) + " aliens")
+for i in range(time):
+    #Prints the population along with the week
+    print("In week " + str(i + 1) + " there are " + str(pop * (2 ** (i + 1))) + " aliens.")
+
